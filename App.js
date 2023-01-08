01176f0ce5a0d2b5d8d59c01703210c8aa625d94
chia7812033 <dan891221@gmail.com>
2023-01-08 20:29:26 +0800

    Render some cards by array

--- App.js
+++ App.js
@@ -2,34 +2,26 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
 
 export default function App() {
+    const cards = data.map(card => {
+        return <Card
+                    img={card.coverImg}
+                    rating={card.stats.rating}
+                    reviewCount={card.stats.reviewCount}
+                    country={card.location}
+                    title={card.title}
+                    price={card.price}
+                />
+    })
     return (
         <div>
             <Navbar />
             <Hero />
             <div className="cards">
-                <Card
-                    img="katie-zaferes.png"
-                    rating="5.0"
-                    reviewCount={6}
-                    country="USA"
-                    title="Life Lessons with Katie Zaferes"
-                    price={136}
-                />
-                <Card
-                    img="katie-zaferes.png"
-                    rating="5.0"
-                    reviewCount={6}
-                    country="USA"
-                    title="Life Lessons with Katie Zaferes"
-                    price={136}
-                />
+                {cards}
             </div>
         </div>
     )
--- index.pack.js
+++ index.pack.js
@@ -411,14 +411,23 @@ var _Card = __webpack_require__(6);
 
 var _Card2 = _interopRequireDefault(_Card);
 
+var _data = __webpack_require__(19);
+
+var _data2 = _interopRequireDefault(_data);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
-
 function App() {
+    var cards = _data2.default.map(function (card) {
+        return _react2.default.createElement(_Card2.default, {
+            img: card.coverImg,
+            rating: card.stats.rating,
+            reviewCount: card.stats.reviewCount,
+            country: card.location,
+            title: card.title,
+            price: card.price
+        });
+    });
     return _react2.default.createElement(
         "div",
         null,
@@ -427,22 +436,7 @@ function App() {
         _react2.default.createElement(
             "div",
             { className: "cards" },
-            _react2.default.createElement(_Card2.default, {
-                img: "katie-zaferes.png",
-                rating: "5.0",
-                reviewCount: 6,
-                country: "USA",
-                title: "Life Lessons with Katie Zaferes",
-                price: 136
-            }),
-            _react2.default.createElement(_Card2.default, {
-                img: "katie-zaferes.png",
-                rating: "5.0",
-                reviewCount: 6,
-                country: "USA",
-                title: "Life Lessons with Katie Zaferes",
-                price: 136
-            })
+            cards
         )
     );
 }
@@ -30658,6 +30652,54 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ }),
+/* 19 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = [{
+    id: 1,
+    title: "Life Lessons with Katie Zaferes",
+    description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+    price: 136,
+    coverImg: "katie-zaferes.png",
+    stats: {
+        rating: 5.0,
+        reviewCount: 6
+    },
+    location: "Online",
+    openSpots: 0
+}, {
+    id: 2,
+    title: "Learn Wedding Photography",
+    description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+    price: 125,
+    coverImg: "wedding-photography.png",
+    stats: {
+        rating: 5.0,
+        reviewCount: 30
+    },
+    location: "Online",
+    openSpots: 27
+}, {
+    id: 3,
+    title: "Group Mountain Biking",
+    description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+    price: 50,
+    coverImg: "mountain-bike.png",
+    stats: {
+        rating: 4.8,
+        reviewCount: 2
+    },
+    location: "Norway",
+    openSpots: 3
+}];
 
 /***/ })
 /******/ ]);
